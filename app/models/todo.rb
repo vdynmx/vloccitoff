4bda7f59e1efbcff60cb6b66cb3db5015218775e
Vincent Fleischacker <vdynmx@gmail.com>
2014-10-23 05:36:27 +0000

    printed date in todolist

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,9 @@ class Todo < ActiveRecord::Base
   def Todo.delete_all_todos
     Todo.where("created_at <= ?", Time.now - 7.days).destroy_all
   end
+
+  def Todo.daysleft
+    Todo.created_at
+  end
+
 end
